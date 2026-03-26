672e8c28b1bb79bc30fc00417eae79f4ebe9a919
David L. Mejia <david.mejia@cobistopaz.com>
2026-03-26 09:43:39 -0500

    Actualizar ruta FE

--- src/main/java/com/libreriaITQ/demo/config/WebConfig.java
+++ src/main/java/com/libreriaITQ/demo/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("https://lemon-water-0d8c33c0f.4.azurestaticapps.net")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
